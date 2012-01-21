836bbb794d7e4a7a5d96b512891d9541b6b2b6ce
Veselin Todorov <veselin@veselin.bg>
2012-01-21 14:53:48 +0200

    Adding answer method to the bot.

--- test/bot.test.js
+++ test/bot.test.js
@@ -12,28 +12,38 @@
  */
 var Bot = require('../lib/bot');
 
+/**
+ * Test dependencies.
+ */
+var Db = require('../lib/db');
+var defaults = require('../lib/db');
+
 describe('Bot', function() {
   describe('version', function() {
-    it('should be sane', function() {
-      var bot = new Bot;
-      bot.version.should.be.ok;
+    it('should be exposed', function() {
+      Bot.version.should.be.ok;
     });
   });
   
   describe('constructor', function() {
-    it('should load db', function() { 
-      var bot = new Bot({foo: 'bar'});
+    it('should load db and defaults', function() { 
+      var bot = new Bot({foo: 'bar'}, ['foo', 'bar']);
       bot._db.should.eql({foo: 'bar'});
+      bot._defaults.should.eql(['foo', 'bar']);
     });
   });
   
   describe('.answer()', function() {
-    it('should answer simple questions', function(done) {
-      var bot = new Bot;
-      bot.answer('Hello!', function(answer) {
-        answer.should.match(/(Hi.|Hello.|Greetings.|How can I help you today?)/);
-        done();
-      });
+    it('should answer questions', function() {
+      var db = new Db;
+      var bot = new Bot(db);
+      bot.answer('Hello.').should.eql('"Hello," he lied.');
+    });
+    
+    it('should answer even if he does not know what to say', function() {
+      var db = new Db;
+      var bot = new Bot(db, ['foo']);
+      bot.answer('dasjdisahdia').should.eql('foo');
     });
   });
 });
